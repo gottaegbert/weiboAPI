6e000b87ade06e828c46e9d765310a236f5ee9e2
upupming <upupming@gmail.com>
2019-06-16 12:54:36 +0800

    Added API: userInfo

---
+++ response.js
@@ -0,0 +1,118 @@
+let requests = [
+  'https://m.weibo.cn/api/container/getIndex?type=uid&value=1669879400'
+];
+let userInfo = {
+  "ok": 1,
+  "data": {
+      "avatar_guide": [],
+      "userInfo": {
+          "id": 1669879400,
+          "screen_name": "Dear-迪丽热巴",
+          "profile_image_url": "https://tvax1.sinaimg.cn/crop.0.0.996.996.180/63885668ly8fjf57kfmgfj20ro0ro0u7.jpg",
+          "profile_url": "https://m.weibo.cn/u/1669879400?uid=1669879400&luicode=10000011&lfid=1005051669879400",
+          "statuses_count": 1008,
+          "verified": true,
+          "verified_type": 0,
+          "verified_type_ext": 1,
+          "verified_reason": "嘉行传媒签约演员　",
+          "close_blue_v": false,
+          "description": "一只喜欢默默表演的小透明。工作联系helloworkwork@126.com🍒",
+          "gender": "f",
+          "mbtype": 12,
+          "urank": 43,
+          "mbrank": 7,
+          "follow_me": false,
+          "following": false,
+          "followers_count": 63357736,
+          "follow_count": 240,
+          "cover_image_phone": "https://tva2.sinaimg.cn/crop.0.0.640.640.640/a1d3feabjw1ecasunmkncj20hs0hsq4j.jpg",
+          "avatar_hd": "https://wx1.sinaimg.cn/orj480/63885668ly8fjf57kfmgfj20ro0ro0u7.jpg",
+          "like": false,
+          "like_me": false,
+          "toolbar_menus": [
+              {
+                  "type": "profile_follow",
+                  "name": "关注",
+                  "pic": "",
+                  "params": {
+                      "uid": 1669879400
+                  }
+              },
+              {
+                  "type": "link",
+                  "name": "聊天",
+                  "pic": "http://h5.sinaimg.cn/upload/2015/06/12/2/toolbar_icon_discuss_default.png",
+                  "params": {
+                      "scheme": "sinaweibo://messagelist?uid=1669879400&nick=Dear-迪丽热巴"
+                  },
+                  "scheme": "https://passport.weibo.cn/signin/welcome?entry=mweibo&r=https%3A%2F%2Fm.weibo.cn%2Fapi%2Fcontainer%2FgetIndex%3Ftype%3Duid%26value%3D1669879400"
+              },
+              {
+                  "type": "link",
+                  "name": "文章",
+                  "pic": "",
+                  "params": {
+                      "scheme": "sinaweibo://cardlist?containerid=2303190002_445_1669879400_WEIBO_ARTICLE_LIST_DETAIL&count=20"
+                  },
+                  "scheme": "https://m.weibo.cn/p/index?containerid=2303190002_445_1669879400_WEIBO_ARTICLE_LIST_DETAIL&count=20&luicode=10000011&lfid=1005051669879400"
+              }
+          ]
+      },
+      "fans_scheme": "https://m.weibo.cn/p/index?containerid=231051_-_fans_intimacy_-_1669879400&luicode=10000011&lfid=1005051669879400",
+      "follow_scheme": "https://m.weibo.cn/p/index?containerid=231051_-_followersrecomm_-_1669879400&luicode=10000011&lfid=1005051669879400",
+      "tabsInfo": {
+          "selectedTab": 1,
+          "tabs": [
+              {
+                  "id": 1,
+                  "tabKey": "profile",
+                  "must_show": 1,
+                  "hidden": 0,
+                  "title": "主页",
+                  "tab_type": "profile",
+                  "containerid": "2302831669879400"
+              },
+              {
+                  "id": 2,
+                  "tabKey": "weibo",
+                  "must_show": 1,
+                  "hidden": 0,
+                  "title": "微博",
+                  "tab_type": "weibo",
+                  "containerid": "1076031669879400",
+                  "apipath": "/profile/statuses",
+                  "url": "/index/my"
+              },
+              {
+                  "id": 4,
+                  "tabKey": "original_video",
+                  "must_show": 0,
+                  "hidden": 0,
+                  "title": "视频",
+                  "tab_type": "video",
+                  "containerid": "2315671669879400"
+              },
+              {
+                  "id": 7,
+                  "tabKey": "super_topic",
+                  "must_show": 0,
+                  "hidden": 0,
+                  "title": "超话",
+                  "tab_type": "cardlist",
+                  "containerid": "2314751669879400"
+              },
+              {
+                  "id": 10,
+                  "tabKey": "album",
+                  "must_show": 0,
+                  "hidden": 0,
+                  "title": "相册",
+                  "tab_type": "album",
+                  "containerid": "1078031669879400"
+              }
+          ]
+      },
+      "scheme": "sinaweibo://userinfo?uid=1669879400&type=uid&value=1669879400&luicode=10000011&lfid=1005051669879400&v_p=42&fid=1005051669879400&uicode=10000011",
+      "showAppTips": 1
+  }
+};
