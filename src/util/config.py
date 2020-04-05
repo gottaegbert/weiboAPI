82b4c6c928547f5b5d3e885a82466b4b0de8f822
gottaegbert <62612263+gottaegbert@users.noreply.github.com>
2020-04-05 17:18:02 +0800

    Update config.py
    change websites

--- src/util/config.py
+++ src/util/config.py
@@ -11,9 +11,11 @@ config = {
         # Cookie 获取方法：前往 m.weibo.cn，打开一条评论较多的微博全文，往下翻几页
         # 这时 Chrome 的 Network 界面的 request headers 就会有 Cookie 信息了
         # 注意: m.weibo.cn 比较特殊，查看微博并不需要登录，而看评论确实是需要的
-        # 比如直接进这个网址 https://m.weibo.cn/detail/4389138709375153，往后多翻几条评论在 Network 的 XHR 里面可以看到 request headers 的 Cookie
-        'COOKIE': 'ALF=1564734503; SCF=AuUY2ywPv1KKDsxqBgngDXYn7XTsKn_5p4iBblRihSxO8mUlZ5DB13iaxpPOY50QQzi_qq8HXRkR0NEl6MjJ-Ts.; SUB=_2A25wGOOGDeRhGeFP4lcU9SfJzD-IHXVT4o3OrDV6PUJbktBeLW_RkW1NQO_UfEy6P_rwgaJHDE-0R3sOldFws7cD; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5aERPgdESA6l4AaFTr3jGy5JpX5K-hUgL.FoMp1K-fSK.fS0e2dJLoIp7LxKML1KBLBKnLxKqL1hnLBoM7SKnRe0eRe0z0; SUHB=0laVFNbqkGjoCM; _T_WM=68656738488; WEIBOCN_FROM=1110106030; MLOGIN=1; XSRF-TOKEN=aa01d1; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D1076031669879400%26uicode%3D20000061%26fid%3D4389138709375153%26oid%3D4389138709375153'
+        # 比如直接进这个网址 https://m.weibo.cn/detail/4461449911415808   //https://m.weibo.cn/detail/4389138709375153，往后多翻几条评论在 Network 的 XHR 里面可以看到 request headers 的 Cookie
+        'COOKIE': 'ALF=1587886750; _T_WM=25387770715; WEIBOCN_FROM=1110006030; MLOGIN=1; XSRF-TOKEN=388e38; SCF=As_-yP-N2m2DH5s6zfyeGjQhu-krcrxuROefVtbVdJ6GKkNRYiwiGQ7mkPAbSZQLEZ0jcEhvrmrx1pFe_-p7sqY.; SUB=_2A25zjdHsDeRhGeRG7lQX-S3EyT6IHXVRcf-krDV6PUJbktAKLVetkW1NUgT7I10jpUYOX8eGJDI2LB4dTSNL2d9g; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhJ93aWUjl1S51Bx.3w6qpi5JpX5K-hUgL.FozRSKqc1KeReoz2dJLoI7LKIgUV9sHX9get; SUHB=010XaB3uzecqOf; SSOLoginState=1586078140; M_WEIBOCN_PARAMS=oid%3D4389138709375153%26lfid%3D2304132803301701_-_WEIBO_SECOND_PROFILE_WEIBO%26luicode%3D20000174%26uicode%3D20000174%26fid%3D102803
     },
+    
+    
     'mysql': {
         'CONNECTION': {
             'host': "localhost",
